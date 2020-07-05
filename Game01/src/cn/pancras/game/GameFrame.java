889b01b5e235f6383338b4141fdb328b0079057d
lvpengze <pengzelv@qq.com>
2020-07-05 20:21:47 +0800

    添加了图片、音频资源文件 实现了图片加载工具类

---
+++ Game01/src/cn/pancras/game/GameFrame.java
@@ -0,0 +1,12 @@
+package cn.pancras.game;
+
+import java.awt.Frame;
+
+/**
+ * 游戏主窗口类
+ *
+ * @author pancras
+ * @version 1.0
+ */
+public class GameFrame extends Frame {
+}
---
+++ Game01/src/cn/pancras/game/GameUtil.java
@@ -0,0 +1,35 @@
+package cn.pancras.game;
+
+import javax.imageio.ImageIO;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * 游戏工具类
+ *
+ * @author pancras
+ * @version 1.0
+ */
+public class GameUtil {
+    private GameUtil() {
+    }
+
+    /**
+     * 返回指定路径文件的图片对象
+     *
+     * @param path 图片路径
+     * @return Image对象
+     */
+    public static Image getImage(String path) {
+        BufferedImage bi = null;
+        try {
+            URL u = GameUtil.class.getResource(path);
+            bi = ImageIO.read(u);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bi;
+    }
+}
